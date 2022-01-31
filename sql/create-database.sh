64c932f758caa499f7555b591fc662198a241a48
Maxim Tsyplakov <maxim.tsyplakov@iva-tech.ru>
2022-01-31 21:24:06 +0300

    drop tables script

--- sql/create-database.sh
+++ sql/create-database.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 PGPASSWORD=postgres psql postgresql://postgres@127.0.0.1 -A -E -t -q -c "CREATE DATABASE cards WITH OWNER cards ENCODING 'UTF8';"
---
+++ sql/drop-tables.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+PGPASSWORD=cards_forever psql postgresql://cards@127.0.0.1/cards -A -E -t -q -c \
+"DROP TABLE dict_ru;
+DROP TABLE dict_en;
+DROP TABLE dict_map_en_ru;"
