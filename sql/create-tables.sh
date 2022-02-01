20acdbae4c573fbba5585e7d82a7dfa011a37135
Maxim Tsyplakov <maxim.tsyplakov@iva-tech.ru>
2022-02-01 06:37:55 +0300

    fix scripts permissions

--- sql/create-tables.sh
+++ sql/create-tables.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PGPASSWORD=cards_forever psql postgresql://cards@127.0.0.1/cards -A -E -t -q -c \
-"CREATE TABLE dict_en(id uuid PRIMARY KEY, word text UNIQUE NOT NULL);
+"CREATE TABLE dict_en(id uuid PRIMARY KEY, word text NOT NULL);
 
-CREATE TABLE dict_ru(id uuid PRIMARY KEY, word text UNIQUE NOT NULL);
+CREATE TABLE dict_ru(id uuid PRIMARY KEY, word text NOT NULL);
 
 CREATE TABLE dict_map_en_ru(id_en uuid NOT NULL,id_ru uuid NOT NULL,
        CONSTRAINT fk_dict_en FOREIGN KEY(id_en) REFERENCES dict_en(id),
