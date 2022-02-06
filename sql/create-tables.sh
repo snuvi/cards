9cc8cd6750560e1dd5a1da5517433e5c87d3e08f
Maxim Tsyplakov <maxim.tsyplakov@iva-tech.ru>
2022-02-06 16:07:47 +0300

    deletion oreder fixed, dictionaries indices added

--- sql/create-tables.sh
+++ sql/create-tables.sh
@@ -7,4 +7,7 @@ CREATE TABLE dict_ru(id uuid PRIMARY KEY, word text NOT NULL);
 CREATE TABLE dict_map_en_ru(id_en uuid NOT NULL,id_ru uuid NOT NULL,
        CONSTRAINT fk_dict_en FOREIGN KEY(id_en) REFERENCES dict_en(id),
        CONSTRAINT fk_dict_ru FOREIGN KEY(id_ru) REFERENCES dict_ru(id)
-);"
+);
+CREATE INDEX dict_en_word_idx ON dict_en (word);
+CREATE INDEX dict_ru_word_idx ON dict_ru (word);
+"
